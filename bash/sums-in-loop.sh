6bd9786ac026ca5779655c82e84a35605fdcab3e
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2022-07-06 16:08:47 -0300

    sum in loop and sums in loop

---
+++ bash/sums-in-loop.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "enter the number of pairs to be added:"
+read N
+
+echo "type a pair of elements (separated by a space) for each line:"
+
+for (( i=0; i < $N; i++ )); do
+  read -a tmp 
+  arr[$i]=$(( ${tmp[0]} + ${tmp[1]} ))
+  unset tmp
+done
+
+echo "array of sums:"
+echo ${arr[@]}
+
