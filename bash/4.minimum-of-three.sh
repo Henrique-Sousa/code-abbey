19f3d7223abe04ac2bddf632314367d123426b38
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2022-07-06 17:01:33 -0300

    minimum of three

---
+++ bash/4.minimum-of-three.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "enter the number of triples to be compared:"
+read -r N
+
+echo "type a triple of elements (separated by spaces) for each line:"
+
+for (( i=0; i < N; i++ )); do
+  read -r -a tmp
+  if [ "${tmp[0]}" -lt "${tmp[1]}" ]; then
+    if [ "${tmp[0]}" -lt "${tmp[2]}" ]; then
+      arr[$i]="${tmp[0]}"
+    else
+      arr[$i]="${tmp[2]}"
+    fi
+  else
+    if [ "${tmp[1]}" -lt "${tmp[2]}" ]; then
+      arr[$i]="${tmp[1]}"
+    else
+      arr[$i]="${tmp[2]}"
+    fi
+  fi
+done
+
+echo "array of minimum elements of each triple:"
+echo "${arr[@]}"
+
