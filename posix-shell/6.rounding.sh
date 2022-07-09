332e120a0960a7f286c92877032ae026ff650e03
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2022-07-09 12:28:55 -0300

    6.rounding.sh

---
+++ posix-shell/6.rounding.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "input data:"
+read -r n
+
+i=1
+while [ "$i" -le "$n" ]; do
+  read -r one two 
+  res=$(
+    bc -s round.bc <<EOF
+    scale=10; r($one / $two)
+EOF
+  )
+  arr="$arr $res"
+  i=$((i + 1))
+done
+
+echo; echo "answer:"
+
+set -- $arr
+
+i=1
+while [ "$i" -le "$n" ]; do
+  eval echo '$'$i 
+  i=$((i + 1))
+done
+
+echo
