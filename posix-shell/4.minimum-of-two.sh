aa4ff5ff0c4722683b6a23dc07635c2a4cbee01a
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2022-07-09 12:02:19 -0300

    4.minimum-of-two.sh

---
+++ posix-shell/4.minimum-of-two.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "enter the number of pairs to be compared:"
+read -r n
+
+echo "type a pair of elements (separated by a space) for each line:"
+
+i=1
+while [ "$i" -le "$n" ]; do
+  read -r one two 
+  if [ "$one" -lt "$two" ]; then
+    arr="$arr $one"
+  else
+    arr="$arr $two"
+  fi
+  i=$((i + 1))
+done
+
+echo "array of minimum elements of each pair:"
+echo "$arr"
