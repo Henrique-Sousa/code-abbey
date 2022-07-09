f718276a78565b86de746b91957121ca9a79914d
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2022-07-09 15:44:39 -0300

    15.maximum-of-array.sh

---
+++ posix-shell/15.maximum-of-array.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo "input data:"
+read -r input
+
+max() {
+  max=$1
+  shift
+  for e in $@; do
+    if [ "$e" -gt "$max" ]; then
+      max="$e"
+    fi
+  done
+
+  echo "$max"
+}
+
+min() {
+  min=$1
+  shift
+  for e in $@; do
+    if [ "$e" -lt "$min" ]; then
+      min="$e"
+    fi
+  done
+
+  echo "$min"
+}
+
+echo; echo "answer:"
+echo "max: $(max $input)"
+echo "min: $(min $input)"
+
