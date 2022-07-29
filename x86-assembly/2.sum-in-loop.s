684db4e83be1e497b1bcd613aea5077e6532b06b
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2022-07-29 20:27:19 -0300

    print result

--- x86-assembly/2.sum-in-loop.s
+++ x86-assembly/2.sum-in-loop.s
@@ -1,25 +1,63 @@
 .section .data
 
+# answer should be: 23632
 array:
-.long 10,20,30,40,5,6,7,8
-
+.long 701, 242, 1246, 506, 29, 459, 1148, 581, 752, 1282, 810, 220, 756, 19, 114, 43, 1182, 634, 61, 164, 821, 29, 1188, 829, 1063, 928, 841, 175, 12, 1069, 1091, 595, 1048, 423, 1149, 785, 637
 size:
-.long 8
+.long 37
+
+.section .bss
+.lcomm answer, 20 
+
+.equ NEWLINE, 10
 
 .section .text
 
 .global _start
 _start:
   movl  $0, %edi
-  movl  $0, %ebx
+  movl  $0, %eax
 
-start_loop:
+sum:
   cmpl  %edi, size
-  je    loop_exit
-  addl  array(,%edi,4), %ebx
+  je    itoa_prep  
+  addl  array(,%edi,4), %eax
   incl  %edi
-  jmp   start_loop
+  jmp   sum
+  
+itoa_prep:
+  xorl %esi, %esi
+itoa:
+  movl $0, %edx
+  movl $10, %ebx
+  divl %ebx
+  addl $48, %edx
+  movl %edx, answer(,%esi,1)
+  incl %esi
+  cmpl $0, %eax
+  jz   print 
+  jmp  itoa 
 
-loop_exit:
-  movl  $1, %eax 
-  int   $0x80
+print:
+  cmpl $0, %esi
+  jz   print_new_line 
+  decl %esi
+  movl $4, %eax
+  leal answer(,%esi,1), %ecx
+  movl $1, %ebx
+  movl $1, %edx
+  int  $0x80
+  jmp  print 
+
+print_new_line:
+  movl $4, %eax
+  pushl $NEWLINE 
+  movl %esp, %ecx 
+  movl $1, %ebx
+  movl $1, %edx
+  int  $0x80
+
+exit:
+  movl $1, %eax
+  int  $0x80
+
